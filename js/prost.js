802ad7752bdfacffbd89027cd8e5b860e8d19d7c
Taras Khomchuk <tarashomchuk@gmail.com>
2025-06-30 22:31:35 +0200

    mobile menu

--- js/prost.js
+++ js/prost.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   const burgerButton = document.getElementById('burger');
+  
+
   if (burgerButton) {
     burgerButton.addEventListener('click', function() {
+        this.classList.toggle('active');
+        const menu = document.getElementById('menu');
+        if (menu) 
+            menu.classList.toggle('menu_mobile');
         
-      console.log('Клік по бургер-меню!'); 
     });
   }
 });
